1d0067a20aa11d1e5fa6793a38d6b5debb1e3f91
a-sohrabi <sohrabi1372@gmail.com>
2021-07-09 23:18:27 +0430

    'second commit'

--- cafe/models.py
+++ cafe/models.py
@@ -10,10 +10,16 @@ class MenuItems(models.Model):
     serving_time_period = models.TimeField()
     estimated_cooking_time = models.TimeField()
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Table(models.Model):
     table_number = models.IntegerField()
     cafe_space_position = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.table_number}"
 
 
 class Orders(models.Model):
@@ -22,6 +28,9 @@ class Orders(models.Model):
     menu_item_id = models.ForeignKey(MenuItems, on_delete=models.CASCADE, related_name='Receipt_User_id')
     time_stamp = models.DateTimeField()
     table_id = models.ForeignKey(Table, on_delete=models.CASCADE, related_name='Receipts_Orders_id')
+
+    def __str__(self):
+        return f"{self.number}"
 
 
 class User(models.Model):
@@ -32,12 +41,18 @@ class User(models.Model):
     birth_day = models.DateField()
     password_to_login = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.last_name}"
+
 
 class Receipts(models.Model):
     timestamp = models.DateTimeField()
     final_price = models.IntegerField()
     total_price = models.IntegerField()
     orders_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.total_price}"
 
 
 
--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+from cafe.models import Orders, MenuItems
+
 
 # Create your views here.
+
+def order_list(request):
+    orders = Orders.objects.all()
+    template = loader.get_template('order_list.html')
+    context = {'orders': orders}
+    return HttpResponse(template.render(context, request))
+
+
+def order_detail(request):
+    order = Orders.objects.get(id=Orders.number)
+    return render()
